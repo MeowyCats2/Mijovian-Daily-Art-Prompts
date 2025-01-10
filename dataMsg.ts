02b9392a2b55035b149dcaa37d710451e27e8fb1
MeowyCats2 <meowycats2@protonmail.com>
2025-01-09 18:02:53 -0800

    Initial commit

---
+++ dataMsg.ts
@@ -0,0 +1,33 @@
+import client from "./client.js";
+import type { TextChannel, Snowflake } from "discord.js";
+
+export const dataMsg = await (await client.channels.fetch("1327066650133925898") as TextChannel).messages.fetch("1327068250848235611");
+const attachmentText = await (await fetch([...dataMsg.attachments.values()][0].url)).text()
+export const dataContent = JSON.parse(attachmentText) as {
+    submissions: {
+        message: Snowflake,
+        user: Snowflake,
+        target: Snowflake
+    }[],
+    current: {
+        submissions: {
+            message: Snowflake,
+            user: Snowflake,
+            target: Snowflake
+        }[],
+        members: {
+            id: Snowflake,
+            displayName: string,
+            username: string
+        }[]
+    },
+    lastRun: string,
+};
+export const saveData = async () => await dataMsg.edit({
+    "files": [
+        {
+            "attachment": Buffer.from(JSON.stringify(dataContent), "utf8"),
+            "name": "data.json"
+        }
+    ]
+});
---
+++ index.ts
@@ -0,0 +1,230 @@
+import client from "./client.ts";
+import { dataContent, saveData } from "./dataMsg.ts"
+import { Routes, ApplicationCommandType, ApplicationCommandOptionType, Events } from "discord.js";
+import type { TextChannel, RESTPutAPIApplicationCommandsJSONBody, GuildMember } from "discord.js"
+import JSZip from "jszip";
+
+const targetChannel = await client.channels.fetch("1327068800931070064") as TextChannel;
+const dataChannel = await client.channels.fetch("1327066650133925898") as TextChannel;
+const getCurrentDate = () => (new Date()).getUTCFullYear() + "-" + (new Date()).getUTCMonth() + "-" + (new Date()).getUTCDate();
+
+const newDay = async () => {
+    const membersCollection = await targetChannel.guild.members.fetch();
+    const members: GuildMember[] = [];
+    while (members.length < 3) {
+        const chosen = membersCollection.random()
+        if (!chosen) throw new TypeError("Member not chosen.")
+        if (chosen.user.bot) continue;
+        members.push(chosen)
+    }
+    let membersString = [];
+    for (const member of members) {
+        membersString.push(`${membersString.length + 1}. <@${member.id}>`)
+    }
+    await targetChannel.send({
+        content: `# M.D.A.P
+    ## Mijovian Daily Art Prompts
+    ## Members
+    ${membersString.join("\n")}
+
+    ## Submitting
+    To submit your art, run the /submit_art command.
+
+    ## Voting`,
+        allowedMentions: {
+            users: []
+        }
+    })
+    for (const submission of dataContent.current.submissions) {
+        const message = await dataChannel.messages.fetch(submission.message);
+        await targetChannel.send({
+            content: `Submitted by: <@${submission.user}>
+    Drawing: <@${submission.target}>`,
+            files: [
+                {
+                    attachment: [...message.attachments.values()][0].url,
+                    name: [...message.attachments.values()][0].name,
+                    description: [...message.attachments.values()][0].description ?? "",
+                }
+            ],
+            allowedMentions: {
+                users: []
+            }
+        });
+    }
+
+    dataContent.submissions.push(...dataContent.current.submissions);
+    dataContent.current.submissions = [];
+    dataContent.lastRun = getCurrentDate();
+    dataContent.current.members = members.map(member => ({
+        id: member.id,
+        displayName: member.displayName,
+        username: member.user.username
+    }))
+    await saveData()
+}
+
+if (dataContent.lastRun !== getCurrentDate()) newDay();
+
+const getMillisecondsUntilMidnight = () => {
+    const now = new Date();
+    const midnight = new Date();
+    midnight.setHours(24, 0, 0, 0);
+    const millisecondsUntilMidnight = midnight.getTime() - now.getTime();
+    return millisecondsUntilMidnight;
+}
+
+setTimeout(() => {
+    newDay();
+    setInterval(newDay, 24 * 60 * 60 * 1000)
+}, getMillisecondsUntilMidnight())
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isAutocomplete()) return;
+    if (interaction.commandName !== "submit_art") return;
+    const name = interaction.options.get("member")?.value as string;
+    await interaction.respond(dataContent.current.members.map((member, index) => ({
+        name: member.displayName + " (" + member.username + ")",
+        value: index + 1
+    })).filter(member => member.name.toLowerCase().includes(name.toLowerCase()) || member.value + "" === name))
+})
+
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+    if (interaction.commandName !== "submit_art") return;
+    await interaction.deferReply();
+    const buffer = await (await fetch(interaction.options.getAttachment("artwork")!.url)).arrayBuffer();
+    const message = await dataChannel.send({
+        files: [
+            {
+                attachment: Buffer.from(buffer),
+                description: interaction.options.getAttachment("artwork")!.description ?? undefined,
+                name: interaction.options.getAttachment("artwork")!.name
+            }
+        ]
+    })
+    dataContent.current.submissions.push({
+        message: message.id,
+        user: interaction.user.id,
+        target: dataContent.current.members[interaction.options.getInteger("member", true) - 1].id
+    })
+    await saveData();
+    await interaction.followUp("Test")
+});
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+    if (interaction.commandName !== "my_art") return;
+    await interaction.deferReply();
+    for (const submission of dataContent.submissions) {
+        if (submission.user !== interaction.user.id) return;
+        const message = await dataChannel.messages.fetch(submission.message);
+        await interaction.followUp({
+            content: `Submitted by: <@${submission.user}>
+Drawing: <@${submission.target}>`,
+            files: [
+                {
+                    attachment: [...message.attachments.values()][0].url,
+                    name: [...message.attachments.values()][0].name,
+                    description: [...message.attachments.values()][0].description ?? "",
+                }
+            ],
+            allowedMentions: {
+                users: []
+            }
+        });
+    }
+    if (dataContent.submissions.filter(submission => submission.user === interaction.user.id).length === 0) return await interaction.followUp("You have no art?")
+})
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+    if (interaction.commandName !== "export_art") return;
+    await interaction.deferReply();
+    if (dataContent.submissions.filter(submission => submission.user === interaction.user.id).length === 0) return await interaction.followUp("You have no art?")
+    const zip = new JSZip();
+    for (const submission of dataContent.submissions) {
+        if (submission.user !== interaction.user.id) return;
+        const message = await dataChannel.messages.fetch(submission.message);
+        zip.file([...message.attachments.values()][0].name, await (await fetch([...message.attachments.values()][0].url)).arrayBuffer())
+    }
+    await interaction.followUp({
+        files: [
+            {
+                attachment: await zip.generateAsync({type: "nodebuffer"}),
+                name: "art.zip"
+            }
+        ]
+    });
+});
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
+    if (interaction.commandName !== "user_submissions") return;
+    await interaction.deferReply();
+    for (const submission of dataContent.submissions) {
+        if (submission.target !== (interaction.options.getMember("member") as GuildMember).id) return;
+        const message = await dataChannel.messages.fetch(submission.message);
+        await interaction.followUp({
+            content: `Submitted by: <@${submission.user}>
+Drawing: <@${submission.target}>`,
+            files: [
+                {
+                    attachment: [...message.attachments.values()][0].url,
+                    name: [...message.attachments.values()][0].name,
+                    description: [...message.attachments.values()][0].description ?? "",
+                }
+            ],
+            allowedMentions: {
+                users: []
+            }
+        });
+    }
+    if (dataContent.submissions.filter(submission => submission.target === (interaction.options.getMember("member") as GuildMember).id).length === 0) return await interaction.followUp("They have no art?")
+})
+const commands: RESTPutAPIApplicationCommandsJSONBody = [
+    {
+        type: ApplicationCommandType.ChatInput,
+        name: "submit_art",
+        description: "Submit a drawing of the members of the day",
+        options: [
+            {
+                type: ApplicationCommandOptionType.Integer,
+                name: "member",
+                description: "The index of the member you're submitting the art for",
+                required: true,
+                min_value: 1,
+                max_value: 3,
+                autocomplete: true
+            },
+            {
+                type: ApplicationCommandOptionType.Attachment,
+                name: "artwork",
+                description: "The artwork you're submitting",
+                required: true
+            }
+        ]
+    },
+    {
+        type: ApplicationCommandType.ChatInput,
+        name: "my_art",
+        description: "See all your submissions"
+    },
+    {
+        type: ApplicationCommandType.ChatInput,
+        name: "user_submissions",
+        description: "See all the drawings of when the user was the target",
+        options: [
+            {
+                type: ApplicationCommandOptionType.User,
+                name: "member",
+                description: "The member to check for",
+                required: true
+            }
+        ]
+    },
+    {
+        type: ApplicationCommandType.ChatInput,
+        name: "export_art",
+        description: "Export all your submissions"
+    },
+]
+if (!client.application) throw new Error("No application for client?")
+await client.rest.put(Routes.applicationCommands(client.application.id), {"body": commands})
